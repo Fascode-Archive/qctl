d2170a3a7a4d7251f643720d013d01d6bf9970bf
hayao <hayao@fascode.net>
2022-01-02 18:55:45 +0900

    Update: Add basic function and messages

--- lib/config.sh
+++ lib/config.sh
@@ -11,20 +11,31 @@ LoadShellFIles "${CrshiniList[@]}"
 # Define functions
 GetConfigFile(){
     local _config=(
-        "${ScriptDir}/../qctl.conf"
+        "${QctlLibDir}/../qctl.conf"
         "${XDG_CONFIG_HOME:-"${HOME}/.config"}/qctl.conf"
         "/etc/qctl.conf"
     )
     local _file
     for _file in "${_config[@]}"; do
         [[ -e "${_file}" ]] && {
+            MsgDebug "Use ${_file} as config file" >&2
             echo "${_file}"
             return 0
         }
     done
+    MsgError "Config file was not found."
+    return 1
 }
 ConfigFile="$(GetConfigFile)"
 
+# GetConfigValue <Section> <Param>
 GetConfigValue(){
-    :
+    _crshini_get "${ConfigFile}" "${@}"
 }
+
+# SetConfigToFile <Section> <Param> <Value>
+SetConfigToFile(){
+    _crshini_set "${ConfigFile}"
+}
+
+
