9c507c32df017f9a0a19cfd081aa15982890a443
hayao <hayao@fascode.net>
2022-01-03 15:35:24 +0900

    Update: Write common codes to qctl-common

--- test.sh
+++ test.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
-
-eval "$(sed "s|%LIB_DIR%|$(cd "$(dirname "${0}")" || exit 1 ; pwd)/lib|g" "${1}")"
+_CurrentDir="$(cd "$(dirname "${0}")" || exit 1 ; pwd)"
+eval "$(
+    sed "$(sed -n "/%COMMON_CODE%/=" "${1}")r ${_CurrentDir}/src/qctl-common" "${1}" | \
+    sed "s|%LIB_DIR%|${_CurrentDir}/lib|g"
+    )"
