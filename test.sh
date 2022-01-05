99eedf62c9bdb7d9bf655c49d3e1ed2d36719787
hayao <hayao@fascode.net>
2022-01-06 00:12:01 +0900

    Fix: Fixed script argument.

--- test.sh
+++ test.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 _CurrentDir="$(cd "$(dirname "${0}")" || exit 1 ; pwd)"
+_TargetScript="${1}"
+shift 1
+
 eval "$(
-    sed "$(sed -n "/%COMMON_CODE%/=" "${1}")r ${_CurrentDir}/src/qctl-common" "${1}" | \
+    sed "$(sed -n "/%COMMON_CODE%/=" "${_TargetScript}")r ${_CurrentDir}/src/qctl-common" "${_TargetScript}" | \
     sed "s|%LIB_DIR%|${_CurrentDir}/lib|g" | \
     grep -xv "%COMMON_CODE%"
     )"
