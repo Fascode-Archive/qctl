f8adcec97de895d34aa8a8010f12502c4f0d8635
hayao <hayao@fascode.net>
2022-01-06 00:10:37 +0900

    Update: Add error if crshini does not return string

--- lib/prepare.sh
+++ lib/prepare.sh
@@ -2,6 +2,11 @@ PrepareConfigDir(){
     local _ConfigDir
     _ConfigDir="$(GetConfigValue "Main" "ConfigDir")"
 
+    [[ -n "${_ConfigDir}" ]] || {
+        MsgError "Failed to get vm directory path."
+        return 1
+    }
+
     [[ -e "${_ConfigDir}" ]] || {
         MsgWarn "${_ConfigDir} was not found. Creating ..."
         mkdir -p "${_ConfigDir}"
