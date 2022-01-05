1529ca48fe3ac8be9f93190273eb0baaafd41904
hayao <hayao@fascode.net>
2022-01-05 22:33:51 +0900

    Fix: Remove %COMMON_CODE%

--- test.sh
+++ test.sh
@@ -2,5 +2,6 @@
 _CurrentDir="$(cd "$(dirname "${0}")" || exit 1 ; pwd)"
 eval "$(
     sed "$(sed -n "/%COMMON_CODE%/=" "${1}")r ${_CurrentDir}/src/qctl-common" "${1}" | \
-    sed "s|%LIB_DIR%|${_CurrentDir}/lib|g"
+    sed "s|%LIB_DIR%|${_CurrentDir}/lib|g" | \
+    grep -xv "%COMMON_CODE%"
     )"
