65064e288c54dbdf918722624af3788bf8c74608
hayao <hayao@fascode.net>
2022-01-06 20:12:18 +0900

    Fix: Fixed common-code replacemet

--- test.sh
+++ test.sh
@@ -3,8 +3,13 @@ _CurrentDir="$(cd "$(dirname "${0}")" || exit 1 ; pwd)"
 _TargetScript="${1}"
 shift 1
 
+_CommonCodeLine="$(sed -n "/%COMMON_CODE%/=" "${_TargetScript}")"
+
 eval "$(
-    sed "$(sed -n "/%COMMON_CODE%/=" "${_TargetScript}")r ${_CurrentDir}/src/qctl-common" "${_TargetScript}" | \
-    sed "s|%LIB_DIR%|${_CurrentDir}/lib|g" | \
+    if [[ -n "${_CommonCodeLine}" ]]; then
+        sed "${_CommonCodeLine}r ${_CurrentDir}/src/qctl-common" "${_TargetScript}"
+    else
+        cat "${_TargetScript}"
+    fi | sed "s|%LIB_DIR%|${_CurrentDir}/lib|g" | \
     grep -xv "%COMMON_CODE%"
     )"
