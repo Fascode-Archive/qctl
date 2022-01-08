75672fcbcc6b842f7c9ff4ca8a070504b2714314
hayao <hayao@fascode.net>
2022-01-08 13:01:18 +0900

    Fix: Create directory

--- lib/disk.sh
+++ lib/disk.sh
@@ -7,10 +7,13 @@ AddNewDisk(){
         MsgError "$_DiskFile does not exist"
         return 1
     }
+
     
     while true; do
         _UUID="$(uuidgen)"
         _Link="${_DiskDir}/${_DiskType}/${_UUID}"
+
+        MakeNewDir "$(dirname "$_Link")"
 
         [[ -e "${_Link}" ]] && continue
         ln -s "${_DiskFile}" "$_Link"
