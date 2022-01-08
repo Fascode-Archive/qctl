a9732175fe5f62ce2b5dd438554b181b881a821c
hayao <hayao@fascode.net>
2022-01-08 23:43:00 +0900

    Update: Add architecture list

--- lib/list.sh
+++ lib/list.sh
@@ -3,7 +3,12 @@ GetVMList(){
 }
 
 GetArchList(){
-    WorkInProgress
+    readarray -t _BinDir < <(
+        while read -r _file; do
+            eval "echo ${_file}"
+        done < <(tr ":" "\n" <<< "${PATH}")
+    )
+    find "${_BinDir[@]}" -name "qemu-system-*" -mindepth 1 -maxdepth 1 -print0 2> /dev/null | xargs -0 -I{} basename {} | sed "s|qemu-system-||g" | sort | uniq
 }
 
 GetVMFileList(){
