74eb26b96c56f7f8edea994944e52299b2c47b80
hayao <hayao@fascode.net>
2022-01-08 13:18:07 +0900

    Update: Add GetPathFromUUID

--- lib/disk.sh
+++ lib/disk.sh
@@ -20,3 +20,24 @@ AddNewDisk(){
         break
     done
 }
+
+# GetPathFromUUID <UUID>
+GetPathFromUUID(){
+    local _DiskDir _UUID="${1-""}"
+    _DiskDir="$(GetDiskDir)"
+
+    [[ -z "${_UUID}" ]] && {
+        MsgError "Usage: GetPathFromUUID <UUID>"
+        return 1
+    }
+
+    readarray -t _UUID_FullPath_List < <(find "${_DiskDir}" -mindepth 2 -maxdepth 2 -type l)
+    readarray -t _UUID_List < <(PrintArray "${_UUID_FullPath_List[@]}" | xargs -I{} basename {})
+
+    PrintArray "${_UUID_List[@]}" | grep -qx "${_UUID}" || {
+        MsgError "No disk with such a UUID was found."
+        return 1
+    }
+
+    readlinkf "$(find "${_DiskDir}" -name "${_UUID}" -mindepth 2 -maxdepth 2 -type l)"
+}
