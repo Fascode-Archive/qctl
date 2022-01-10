2d094198dfc7ef4c7a8f3f97b0acf7bb28c889b7
hayao <hayao@fascode.net>
2022-01-10 15:57:43 +0900

    Update: Add test.sh usage

--- test.sh
+++ test.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
+set -eu
+
 _CurrentDir="$(cd "$(dirname "${0}")" || exit 1 ; pwd)"
-_TargetScript="${1}"
+_TargetScript="${1-""}"
+
+[[ -n "${_TargetScript}" ]] || {
+    echo "Usage: test.sh <script> <script args> ..." >&2
+    exit 1
+}
+
 shift 1
 
 _CommonCodeLine="$(sed -n "/%COMMON_CODE%/=" "${_TargetScript}")"
