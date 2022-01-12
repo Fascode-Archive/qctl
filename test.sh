cc5930ab1761e968f49d7755e611d80ff4a2857c
hayao <hayao@fascode.net>
2022-01-12 23:15:08 +0900

    Update: Run script via build.sh

--- test.sh
+++ test.sh
@@ -9,28 +9,6 @@ _TargetScript="${1-""}"
     exit 1
 }
 
-_Remove_Line=(
-    "#!/usr/bin/env bash"
-    "%COMMON_CODE%"
-)
-
 shift 1
 
-_CommonCodeLine="$(sed -n "/%COMMON_CODE%/=" "${_TargetScript}")"
-readarray -t _Grep_Args < <(
-    for _Str in "${_Remove_Line[@]}"; do
-        printf -- "-e\n"
-        echo "${_Str}"
-    done
-)
-
-eval "$(
-    if [[ -n "${_CommonCodeLine}" ]]; then
-        sed "${_CommonCodeLine}r ${_CurrentDir}/src/qctl-common" "${_TargetScript}"
-    else
-        cat "${_TargetScript}"
-    fi | \
-    sed "s|%LIB_DIR%|${_CurrentDir}/lib|g" | \
-    grep -Fxv "${_Grep_Args[@]}" | \
-    sed "s|%DATA_DIR%|${_CurrentDir}/data|g"
-    )"
+eval "$("${_CurrentDir}/build.sh" -b "/" -l "${_CurrentDir}/lib" -s "${_TargetScript}")"
