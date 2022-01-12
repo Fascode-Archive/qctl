1b0f590021f4c42cbb659d797d323731bad72ef8
hayao <hayao@fascode.net>
2022-01-12 16:53:42 +0900

    Add: Add new build.sh

--- build.sh
+++ build.sh
@@ -1,22 +1,75 @@
 #!/usr/bin/env bash
 
-_CurrentDir="$(cd "$(dirname "${0}")" || exit 1 ; pwd)"
+set -eu
 
-EtcDir=/etc/qctl
-LibDir=/usr/lib/qctl/
-BinDir=/usr/bin/
-DESTDIR="${1}"
+CurrentDir="$(cd "$(dirname "${0}")" || exit 1 ; pwd)"
 
-mkdir -p "${DESTDIR}/$BinDir" "${DESTDIR}/${LibDir}"
+QctlLibDir="${CurrentDir}/lib"
+DataDir="$CurrentDir/data"
 
-while read -r file; do
-    sed "$(sed -n "/%COMMON_CODE%/=" "${file}")r ${_CurrentDir}/src/qctl-common" "${file}" | \
-    grep -xv "%COMMON_CODE%" | \
-    sed "s|%LIB_DIR%|${DESTDIR}/${LibDir}|g" > "${DESTDIR}/${BinDir}/$(basename "${file}")"
-    chmod 755 "${DESTDIR}/${BinDir}/$(basename "${file}")"
-done < <(find "${_CurrentDir}/src/" -mindepth 1)
+LoadShells=("${QctlLibDir}/main.sh")
 
-while read -r file; do
-    cat "${file}" > "${DESTDIR}/${LibDir}/$(basename "${file}")"
-    chmod 755 "${DESTDIR}/${LibDir}/$(basename "${file}")"
-done < <(find "${_CurrentDir}/lib/" -mindepth 1)
+# Load main library
+for file in "${LoadShells[@]}"; do
+    [[ -e "${file}" ]] && source "${file}"
+done
+
+: "${BaseDir="/"}"
+: "${EtcDir="/etc"}"
+: "${LibDir="/lib"}"
+: "${BinDir="/bin"}"
+
+
+HelpDoc(){
+    echo "usage build.sh [option]"
+    echo
+    echo "Build qctl and install to the dir"
+    echo
+    echo " Install options:"
+    echo "    -s | --script [path]     Build the specified script"
+    echo "                             Script will be outputed to stdin"
+    echo "    -i | --install           Build all scripts and install"
+    echo "                             Outout directory can be specified"
+    echo
+    echo " Instal options:"
+    echo "    -b | --basedir [dir]     The path of root. etc, bin and lib files will be installed here"
+    echo "                             Default: ${BaseDir-""}"
+    echo "    -e | --etcdir [dir]      The path of config directory under the base dir"
+    echo "                             Default: ${EtcDir-""}"
+    echo "    -l | --libdir [dir]      The path of library directory under the base dir"
+    echo "                             Default: ${LibDir-""}"
+    echo "    -c | --bindir [dir]      The path of commands directory under the base dir"
+    echo "                             Default: ${BinDir-""}"
+    echo
+    echo " General options:"
+    echo "    -h          This help message"
+}
+
+Main(){
+    :
+}
+
+# Parse options
+OPTS=("s:" "i" "b:" "e:" "l:" "c:" "h") OPTL=("script:" "install" "basedir:" "etcdir:" "libdir:" "bindir:" "help")
+ParseCmdOpt SHORT="$(printf "%s" "${OPTS[@]}")" LONG="$(printf "%s," "${OPTL[@]}")" -- "${@}" || exit 1
+eval set -- "${OPTRET[@]}"
+unset OPTRET OPTS OPTL
+
+while true; do
+    case "${1}" in
+        -h | --help)
+            HelpDoc
+            exit 0
+            ;;
+        --)
+            shift 1
+            break
+            ;;
+        *)
+            MsgError "Argument exception error '${1}'"
+            MsgError "Please report this error to the developer." 1
+            ;;
+    esac
+done
+
+Main "${@}"
