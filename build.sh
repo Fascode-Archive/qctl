ae887e3cf82c8d695cee5763f31c2a22741e91bb
hayao <hayao@fascode.net>
2022-01-12 22:43:37 +0900

    Update: Add BuildScript and RunInstall

--- build.sh
+++ build.sh
@@ -4,10 +4,18 @@ set -eu
 
 CurrentDir="$(cd "$(dirname "${0}")" || exit 1 ; pwd)"
 
-QctlLibDir="${CurrentDir}/lib"
-DataDir="$CurrentDir/data"
+SrcLibDir="${CurrentDir}/lib"
+SrcDataDir="$CurrentDir/data"
 
-LoadShells=("${QctlLibDir}/main.sh")
+QctlLibDir="$SrcLibDir"
+LoadShells=("${SrcLibDir}/main.sh")
+
+Remove_Line=(
+    "#!/usr/bin/env bash"
+    "%COMMON_CODE%"
+)
+
+ExcludeBase=false
 
 # Load main library
 for file in "${LoadShells[@]}"; do
@@ -15,9 +23,10 @@ for file in "${LoadShells[@]}"; do
 done
 
 : "${BaseDir="/"}"
-: "${EtcDir="/etc"}"
-: "${LibDir="/lib"}"
+: "${EtcDir="/etc/qctl"}"
+: "${LibDir="/lib/qctl"}"
 : "${BinDir="/bin"}"
+: "${DataDir="/usr/share/qctl"}"
 
 BuildMode=""
 TargetPath=""
@@ -42,17 +51,101 @@ HelpDoc(){
     echo "                             Default: ${LibDir-""}"
     echo "    -c | --bindir [dir]      The path of commands directory under the base dir"
     echo "                             Default: ${BinDir-""}"
+    echo "    -d | --datadir [dir]     The path of misc data directory under the base dir"
+    echo "                             Default: ${DataDir-""}"
+    echo "         --excludebase       Specify the path including basedir in the script"
+    echo "                             Please read following message for detail"
+    echo
+    echo " About \"--excludebase\""
+    echo "By default, installed scripts use the path containing basedir. "
+    echo "This is useful if you are installing directly on your system."
+    echo "Specify this option if you do not want the directory name to include basedir for reasons such as packaging."
+    echo 
+    echo "For example, if you use this option, the library itself will be installed in <basedir>/<libdir>, "
+    echo "but the actual command will try to load <libdir>."
     echo
     echo " General options:"
     echo "    -h          This help message"
 }
 
+BuildScript(){
+    # 削除する行の一覧
+    readarray -t _Grep_Args < <(
+        for _Str in "${Remove_Line[@]}"; do
+            printf -- "-e\n"
+            echo "${_Str}"
+        done
+    )
+
+    # CommonCodeの挿入箇所
+    local _CommonCodeLine
+    _CommonCodeLine="$(sed -n "/%COMMON_CODE%/=" "${1}")"
+
+    grep -Fxv "${_Grep_Args[@]}" "${1}" | \
+    if [[ -n "${_CommonCodeLine}" ]]; then
+        sed "${_CommonCodeLine}r ${CurrentDir}/src/qctl-common"
+    else
+        cat "${1}"
+    fi | \
+    sed '/^$/d' | \
+    if [[ "${ExcludeBase}" = true ]]; then
+        sed "s|%LIB_DIR%|${LibDir}|g" | \
+        sed "s|%DATA_DIR%|${DataDir}|g"
+    else
+        sed "s|%LIB_DIR%|${LibFullPath}|g" | \
+        sed "s|%DATA_DIR%|${DataFullPath}|g"
+    fi
+}
+
+RunInstall(){
+    local _Dir
+    for _Dir in Etc Bin Data Lib; do
+        eval "mkdir -p \"\${${_Dir}FullPath}\""
+        eval "${_Dir}FullPath=\"\$(readlinkf \"\${${_Dir}FullPath}\")\""
+    done
+    
+
+    EtcFullPath=$(readlinkf "${EtcFullPath}")
+
+    # Install /src/ to /bin
+    local _File
+    while read -r _File; do
+        BuildScript "${_File}" > "${BinFullPath}/$(basename "${_File}")"
+        chmod 755 "${BinFullPath}/$(basename "${_File}")"
+    done < <(find "${CurrentDir}/src/" -mindepth 1)
+
+    # Install /lib to /lib
+    while read -r _File; do
+        cat "${_File}" > "${LibFullPath}/$(basename "${_File}")"
+        chmod 755 "${LibFullPath}/$(basename "${_File}")"
+    done < <(find "${CurrentDir}/lib/" -mindepth 1)
+
+    # Install qctl.conf to /etc
+    cat "$CurrentDir/qctl.conf" > "${EtcFullPath}/qctl.conf"
+
+    # Install /data to /usr/share
+    while read -r _File; do
+        cp "${_File}" "${DataFullPath}"
+    done < <(find "${CurrentDir}/data" -mindepth 1)
+}
+
 Main(){
-    :
+    case "${BuildMode}" in
+        "Script")
+            BuildScript "${TargetPath}"
+            ;;
+        "Install")
+            RunInstall
+            ;;
+        "")
+            MsgError "There is nothing to do."
+            exit 0
+            ;;
+    esac
 }
 
 # Parse options
-OPTS=("s:" "i" "b:" "e:" "l:" "c:" "h") OPTL=("script:" "install" "basedir:" "etcdir:" "libdir:" "bindir:" "help")
+OPTS=("s:" "i" "b:" "e:" "l:" "c:" "h") OPTL=("script:" "install" "basedir:" "etcdir:" "libdir:" "bindir:" "help" "excludebase") 
 ParseCmdOpt SHORT="$(printf "%s" "${OPTS[@]}")" LONG="$(printf "%s," "${OPTL[@]}")" -- "${@}" || exit 1
 eval set -- "${OPTRET[@]}"
 unset OPTRET OPTS OPTL
@@ -62,7 +155,7 @@ while true; do
         -s | --script)
             BuildMode="Script"
             TargetPath="${2}"
-            shift 1
+            shift 2
             ;;
         -i | --install)
             BuildMode="Install"
@@ -84,9 +177,17 @@ while true; do
             BinDir="$2"
             shift 2
             ;;
+        -d | --datadir)
+            DataDir="$2"
+            shift 2
+            ;;
         -h | --help)
             HelpDoc
             exit 0
+            ;;
+        --excludebase)
+            ExcludeBase=true
+            shift 1
             ;;
         --)
             shift 1
@@ -98,5 +199,15 @@ while true; do
             ;;
     esac
 done
+
+#EtcFullPath="$(readlinkf "$BaseDir/$EtcDir")"
+#LibFullPath="$(readlinkf "$BaseDir/$LibDir")"
+#BinFullPath="$(readlinkf "$BaseDir/$BinDir")"
+#DataFullPath="$(readlinkf "$BaseDir/$DataDir")"
+
+EtcFullPath="$BaseDir/$EtcDir"
+LibFullPath="$BaseDir/$LibDir"
+BinFullPath="$BaseDir/$BinDir"
+DataFullPath="$BaseDir/$DataDir"
 
 Main "${@}"
